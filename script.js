59971416c3529d20371a9512ad7508cfd68da6a4
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-25 10:41:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const nameBox = document.getElementById(`fname`);
+
+nameBox.addEventListener(`input`, () => {
+    const text = nameBox.value;
+    nameBox.value = text.toUpperCase();
+
+    return;
+});
